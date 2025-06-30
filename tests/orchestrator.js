c8c36eab8e78a8d081f60f4bbbfe5ecf626763b1
Filipe Amaro Cinque Santana <97483586+SrCinque@users.noreply.github.com>
2025-06-30 21:43:34 +0000

    adicionando novas modificações

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,11 +5,14 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fecthStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     });
 
     async function fecthStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+      if (response.status != 200) {
+        throw Error();
+      }
     }
   }
 }
