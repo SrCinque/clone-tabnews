c65c37bdecca77263f8f01e07d912027082ad60c
Filipe Amaro Cinque Santana <97483586+SrCinque@users.noreply.github.com>
2025-08-21 23:41:40 +0000

    feat: adicção de erros

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,35 +1,38 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
-  const databaseVersionResult = await database.query("SHOW server_version;");
-  const databseVersionValue = databaseVersionResult.rows[0].server_version;
+  try {
+    const updatedAt = new Date().toISOString();
+    const databaseVersionResult = await database.query("SHOW server_version;");
+    const databseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionResult = await database.query(
-    "SHOW max_connections;",
-  );
-  const databaseMaxConnectionValue =
-    databaseMaxConnectionResult.rows[0].max_connections;
+    const databaseMaxConnectionResult = await database.query(
+      "SHOW max_connections;",
+    );
+    const databaseMaxConnectionValue =
+      databaseMaxConnectionResult.rows[0].max_connections;
 
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseopenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseopenedConnectionsResult = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
 
-  const databaseopenedConnectionsValue =
-    databaseopenedConnectionsResult.rows[0].count;
+    const databaseopenedConnectionsValue =
+      databaseopenedConnectionsResult.rows[0].count;
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependecies: {
-      database: {
-        version: databseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionValue),
-        opened_connections: databaseopenedConnectionsValue,
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependecies: {
+        database: {
+          version: databseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionValue),
+          opened_connections: databaseopenedConnectionsValue,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {}
 }
 
 export default status;
