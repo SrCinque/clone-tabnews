cad373b7c7a3b989e0bbcf4f0c51e29e503b9e19
Filipe Amaro Cinque Santana <97483586+SrCinque@users.noreply.github.com>
2025-09-16 14:57:48 +0000

    feat: create `user.findOneByUsername` meathod and `/api/v1/users/[username]` controller

--- models/user.js
+++ models/user.js
@@ -1,5 +1,34 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findOneByUsername(username) {
+  const userfound = await runSelectQuery(username);
+
+  return userfound;
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+        SELECT 
+          *
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT 
+          1    
+        ;`,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "Usuário não encontrado",
+        action: "Verifique o username e tente novamente.",
+      });
+    }
+    return result.rows[0];
+  }
+}
 
 async function create(inputValues) {
   await validateUniqueEmail(inputValues.email);
@@ -64,6 +93,7 @@ async function create(inputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
