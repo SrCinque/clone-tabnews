4276fc11fb1991e091c0cb251a44fd53c8a160b1
Filipe Amaro Cinque Santana <97483586+SrCinque@users.noreply.github.com>
2025-11-02 22:01:47 +0000

    feat: add initial file's of get user info's and revalidantion of sessions

--- models/session.js
+++ models/session.js
@@ -3,6 +3,32 @@ import database from "infra/database";
 
 const EXPIRANTION_IN_MILLISECONDS = 60 * 60 * 24 * 30 * 1000; // 30 dias
 
+async function findOneValidByToken(sessionToken) {
+  const sessionFound = await selectQuery(sessionToken);
+
+  return sessionFound;
+
+  async function selectQuery(sessionToken) {
+    const result = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        sessions
+      WHERE
+        token = $1
+      AND 
+        expires_at > NOW()
+      LIMIT
+        1
+    `,
+      values: [sessionToken],
+    });
+
+    return result.rows[0];
+  }
+}
+
 async function create(userId) {
   const token = cripto.randomBytes(48).toString("hex");
   const expiresAt = new Date(Date.now() + EXPIRANTION_IN_MILLISECONDS);
@@ -28,6 +54,7 @@ async function create(userId) {
 
 const session = {
   create,
+  findOneValidByToken,
   EXPIRANTION_IN_MILLISECONDS,
 };
 
---
+++ tests/integration/api/v1/user/get.test.js
@@ -0,0 +1,40 @@
+import orchestrator from "tests/orchestrator.js";
+import { version as uuidVersion } from "uuid";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.claerDatabase();
+  await orchestrator.runPeddingMigrations();
+});
+
+describe("GET to api/v1/user", () => {
+  describe("Default user", () => {
+    test("Witch valid session", async () => {
+      const createdUser = await orchestrator.createUser({
+        username: "UserValidSession",
+      });
+
+      const sessionObject = await orchestrator.createSession(createdUser.id);
+
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      });
+
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        id: createdUser.id,
+        username: "UserValidSession",
+        email: createdUser.email,
+        password: createdUser.password,
+        created_at: createdUser.created_at.toISOString(),
+        updated_at: createdUser.updated_at.toISOString(),
+      });
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+  });
+});
